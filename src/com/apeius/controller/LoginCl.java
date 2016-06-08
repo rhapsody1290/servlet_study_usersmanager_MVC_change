a6f5b4695c37abddbb10c2ff478690be63ef3d16
rhapsody1290 <369201191@qq.com>
2016-06-08 11:33:34 +0800

    姓名，密码，性别，爱好，城市，个人介绍

--- src/com/apeius/controller/LoginCl.java
+++ src/com/apeius/controller/LoginCl.java
@@ -4,6 +4,7 @@ import com.apeius.domain.User;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Enumeration;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -14,17 +15,26 @@ public class LoginCl extends HttpServlet {
 
 	public void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-
+		request.setCharacterEncoding("utf-8");
 		response.setContentType("text/html;charset=utf-8");
 		response.setCharacterEncoding("utf-8");
 		PrintWriter out = response.getWriter();
+
 		String username = request.getParameter("username");
 		String password = request.getParameter("password");
-
-		if("qm".equals(username) && "123".equals(password)){
+		String sex = request.getParameter("sex");
+		String[] hobby = request.getParameterValues("hobby");
+		String city = request.getParameter("city");
+		String intro = request.getParameter("intro");
+		if(username != null && password != null){
 			//跳转到下一个页面，servlet提供了两种，sendRedirect 转向 和 forward 转发
 			//sendRediret的url应该/web应用名/servlet
 			request.getSession().setAttribute("username",username);
+			request.getSession().setAttribute("password",password);
+			request.getSession().setAttribute("sex",sex);
+			request.getSession().setAttribute("hobby",hobby);
+			request.getSession().setAttribute("city",city);
+			request.getSession().setAttribute("intro",intro);
 			response.sendRedirect("/UsersManager/MainFrame");
 		}else{
 			response.sendRedirect("/UsersManager/Login");
--- src/com/apeius/view/MainFrame.java
+++ src/com/apeius/view/MainFrame.java
@@ -12,12 +12,26 @@ public class MainFrame extends HttpServlet {
 
 	public void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-
+		request.setCharacterEncoding("utf-8");
 		response.setContentType("text/html;charset=utf-8");
 		response.setCharacterEncoding("utf-8");
 		PrintWriter out = response.getWriter();
 		out.println("<h1>主界面</h1>");
 		out.println("<p>username:"+request.getSession().getAttribute("username")+"</p>");
+		out.println("<p>password:"+request.getSession().getAttribute("password")+"</p>");
+		out.println("<p>sex:"+request.getSession().getAttribute("sex")+"</p>");
+		String[] hobby = (String[])request.getSession().getAttribute("hobby");
+		if(hobby != null){
+			out.println("<p>你的爱好：");
+			for(String s : hobby){
+				out.println(s);
+			}
+			out.println("</p>");
+		}else{
+			out.println("<p>你没有选择爱好</p>");
+		}
+		out.println("<p>城市:"+request.getSession().getAttribute("city")+"</p>");
+		out.println("<p>自我介绍:"+request.getSession().getAttribute("intro")+"</p>");
 	}
 
 	public void doPost(HttpServletRequest request, HttpServletResponse response)
