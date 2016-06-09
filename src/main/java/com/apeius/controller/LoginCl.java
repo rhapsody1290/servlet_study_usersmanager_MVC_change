297d046440448cfe779f9199b8f19a8ece358bac
rhapsody1290 <369201191@qq.com>
2016-06-09 15:57:22 +0800

    数据库验证用户

--- src/main/java/com/apeius/controller/LoginCl.java
+++ src/main/java/com/apeius/controller/LoginCl.java
@@ -1,15 +1,15 @@
 package com.apeius.controller;
 
-import com.apeius.domain.User;
-
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.Enumeration;
-
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 public class LoginCl extends HttpServlet {
 
@@ -35,8 +35,66 @@ public class LoginCl extends HttpServlet {
 		* 4.执行操作
 		* 5.根据结果做处理
 		* */
+		Connection connection = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
 
-		if(username != null && password != null){
+		try {
+			//1.加载驱动
+			Class.forName("com.mysql.jdbc.Driver");
+			//2.获得连接
+			connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/servlet_users_manager","root","root");
+			//3.创建prepareStatement
+			ps = connection.prepareStatement("select * from users where username = ? and passwd = ?");
+			ps.setObject(1,username);
+			ps.setObject(2,password);
+			//4.执行操作
+			rs = ps.executeQuery();
+			//5.根据结果做处理
+			if(rs.next()){
+				//说明用户合法
+
+				//跳转到下一个页面，servlet提供了两种，sendRedirect 转向 和 forward 转发
+				//sendRediret的url应该/web应用名/servlet
+				request.getSession().setAttribute("username",username);
+				request.getSession().setAttribute("password",password);
+				request.getSession().setAttribute("sex",sex);
+				request.getSession().setAttribute("hobby",hobby);
+				request.getSession().setAttribute("city",city);
+				request.getSession().setAttribute("intro",intro);
+				request.getRequestDispatcher("/MainFrame").forward(request,response);
+			}else{
+				request.getRequestDispatcher("/Login").forward(request,response);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			//关闭资源
+			if(rs != null){   // 关闭记录集
+				try{
+					rs.close();
+				}catch(Exception e){
+					e.printStackTrace() ;
+				}
+			}
+			if(ps != null){   // 关闭声明
+				try{
+					ps.close() ;
+				}catch(Exception e){
+					e.printStackTrace() ;
+				}
+			}
+			if(connection != null){  // 关闭连接对象
+				try{
+					connection.close() ;
+				}catch(Exception e){
+					e.printStackTrace() ;
+				}
+			}
+		}
+
+
+		/*if(username != null && password != null){
 			//跳转到下一个页面，servlet提供了两种，sendRedirect 转向 和 forward 转发
 			//sendRediret的url应该/web应用名/servlet
 			request.getSession().setAttribute("username",username);
@@ -48,7 +106,7 @@ public class LoginCl extends HttpServlet {
 			response.sendRedirect("/UsersManager/MainFrame");
 		}else{
 			response.sendRedirect("/UsersManager/Login");
-		}
+		}*/
 	}
 
 	public void doPost(HttpServletRequest request, HttpServletResponse response)
